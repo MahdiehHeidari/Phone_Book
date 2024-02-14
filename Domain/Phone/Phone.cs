097ce33fd25174da413218c11ffb1f8aed99ae8e
mahdieh1374 <mahdiehheidari93@gmail.com>
2024-02-15 00:43:45 +0330

    Phone Model and annotations

--- Domain/Phone/Phone.cs
+++ Domain/Phone/Phone.cs
@@ -1,10 +1,23 @@
-﻿namespace Domain.Phone
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Domain.PhoneTypes;
+using Domain.Persons;
+namespace Domain.Phones
 {
 	public class Phone
 	{
-		public Phone()
-		{
-		}
-	}
+        [Required, Key]//not null,primary key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(14, MinimumLength = 11)]
+        [RegularExpression(@"^\d+$", ErrorMessage = "شماره تلفن باید فقط شامل اعداد باشد.")]
+        public string PhoneNumber { get; set; }
+
+
+        public PhoneType Type { get; set; }
+        public int PersonId { get; set; } // Foreign key to Person
+        public Person Person { get; set; } // Navigation property
+    }
 }
 
